644bcf96ee62e651372d458933ab666a2b862311
lorenacmora <lcma1981@gmail.com>
2023-03-08 22:37:46 +0000

    cambios 2

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -10,7 +10,7 @@ export const Navbar = () => {
       <Link to="/">
         <img
           src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.allocine.fr%2Farticle%2Ffichearticle_gen_carticle%3D18637936.html&psig=AOvVaw1D1G3PssCBAtZknytpc43m&ust=1678396907220000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPijp-yhzf0CFQAAAAAdAAAAABAb" height={50} width={60}
-          " />
+           />
       </Link>
       <div className="ml-auto ">
         <div className="dropdown ">
